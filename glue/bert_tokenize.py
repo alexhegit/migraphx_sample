38725b83d9b958508f9540db3218d6452afc3db4
Mike Vermeulen <mike@vermeulen.com>
2019-07-23 17:16:11 -0500

    Add comment to the bert_tokenize.py

--- glue/bert_tokenize.py
+++ glue/bert_tokenize.py
@@ -1,5 +1,7 @@
 from pytorch_transformers import BertTokenizer
 
+# Note: A more complete example is the "convert_examples_to_features" in the utils_glue.py script.
+
 tokenizer = BertTokenizer.from_pretrained("bert-base-cased",do_lower_case=False)
 tokens = tokenizer.tokenize("The rain in Spain falls mainly on the plain.")
 token_ids = list(map(tokenizer.convert_tokens_to_ids,tokens))
