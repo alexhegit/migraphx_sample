ae8aae36222c02c769a67956da4f8784f027d51e
Mike Vermeulen <mike@vermeulen.com>
2019-07-24 11:29:16 -0500

    Add file to generate TST files.

--- glue/glue_dump.sh
+++ glue/glue_dump.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Dump output lines from GLUE .tsv files
+# By default, dumps into ASCII format, but when GLUE_TST variable is set dumps to the following format:
+#
+# <label>\t<tokens1>\t<tokens2>
+#
+# where <tokens1> and <tokens2> are comma separated.
 GLUE_DATA=${GLUE_DATA:="./glue_data"}
 GLUE_TASK=${GLUE_TASK:="CoLA"}
+GLUE_TST=${GLUE_TST:=0}
 
 case ${GLUE_TASK} in
 # Single Sentence Tasks
@@ -9,13 +16,13 @@ case ${GLUE_TASK} in
     # Single sentences annotated whether they are gramatical sentences
     CoLA)
 	validation_file=${GLUE_DATA}/CoLA/dev.tsv
-	awk_fields='{ print "label="$2 "\tsentence="$4 }'
+	awk_fields='{ print $2 "|" $4 }'
 	;;
     # Stanford Sentiment Treebank
     # Single sentences from movie reviews. Annotated whether they are positive or negative.
     SST)
 	validation_file=${GLUE_DATA}/SST-2/dev.tsv
-	awk_fields='{ print "label="$2 "\tsentence="$1 }'
+	awk_fields='{ print $2 "|" $1 }'
 	;;
 
 # Similarity and paraphrase tasks    
@@ -24,7 +31,7 @@ case ${GLUE_TASK} in
     # Unbalanced (68% positive)
     MRPC)
 	validation_file=${GLUE_DATA}/MRPC/dev.tsv
-	awk_fields='{ print "label="$1 "\n\tsentence="$4 "\n\tsentence2="$5 }'
+	awk_fields='{ print $1 "|" $4 "|" $5 }'
 	;;
 
     # Quora Question Pairs
@@ -32,14 +39,14 @@ case ${GLUE_TASK} in
     # Unbalanced (63% negative)
     QQP)
 	validation_file=${GLUE_DATA}/QQP/dev.tsv
-	awk_fields='{ print "label="$6 "\n\tsentence="$4 "\n\tsentence2="$5 }'
+	awk_fields='{ print $6 "|" $4 "|" $5 }'	
 	;;
 
     # Semantic Textual Similarity
     # Sentence pairs drawn from news headlines, are they similar? (0 to 5)
     STS)
-	validation_file=${GLUE_DATA}/STS-B/dev.tsv	
-	awk_fields='{ print "label="$10 "\n\tsentence="$8 "\n\tsentence2="$9 }'
+	validation_file=${GLUE_DATA}/STS-B/dev.tsv
+	awk_fields='{ print $10 "|" $8 "|" $9 }'
 	;;
 
 # Inference tasks    
@@ -48,28 +55,28 @@ case ${GLUE_TASK} in
     # Looks at both matched (in domain) and mis-matched (cross-domain)
     MNLI)
 	validation_file=${GLUE_DATA}/MNLI/dev_matched.tsv
-	awk_fields='{ print "label="$16 "\n\tsentence="$9 "\n\tsentence2="$10 }'
+	awk_fields='{ print $16 "|" $9 "|" $10 }'
 	;;
 
     # Stanford Question Answering Dataset
     # Does the second sentence answer the question?
     QNLI)
 	validation_file=${GLUE_DATA}/QNLI/dev.tsv
-	awk_fields='{ print "label="$4 "\n\tquestion="$2 "\n\tsentence2="$3 }'
+	awk_fields='{ print $4 "|" $2 "|" $3 }'
 	;;
 
     # Recognizing Textual Entailment
     # Does the second sentence agree with the first
     RTE)
 	validation_file=${GLUE_DATA}/RTE/dev.tsv
-	awk_fields='{ print "label="$4 "\n\tsentence="$2 "\n\tsentence2="$3 }'
+	awk_fields='{ print $4 "|" $2 "|" $3 }'	
 	;;
 
     # Winograd Schema Challenge
     # Does the second sentence have the proper pronoun replacement
     WNLI)
 	validation_file=${GLUE_DATA}/WNLI/dev.tsv
-	awk_fields='{ print "label="$4 "\n\tsentence="$2 "\n\tsentence2="$3 }'
+	awk_fields='{ print $4 "|" $2 "|" $3 }'		
 	;;    
 
     
@@ -78,5 +85,14 @@ case ${GLUE_TASK} in
 	exit 1
 esac
 
-cat $validation_file | awk -F '\t' "$awk_fields"
-
+IFS='|'
+cat $validation_file | awk -F '\t' "$awk_fields" | while read label sentence1 sentence2
+do
+    if [ "${GLUE_TST}" == "0" ]; then
+	echo "label=$label"
+	echo "   sentence1=$sentence1"
+	echo "   sentence2=$sentence2"
+    else
+	echo "$label	"`echo $sentence1|./tokenize.sh`"	"`echo $sentence2|./tokenize.sh`
+    fi
+done
